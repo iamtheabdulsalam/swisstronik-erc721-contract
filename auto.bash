3ad20502b7c3e289859ab9a8d1a0fd6b4872ecd1
Abdulsalam Abdulsalam <0xdudul@gmail.com>
2024-08-18 14:34:32 +0000

    feat: implemented erc721

---
+++ auto.bash
@@ -0,0 +1,21 @@
+npm i
+
+npm run compile
+
+npm run deploy
+npm run deploy
+npm run deploy
+npm run deploy
+npm run deploy
+
+npm run mint
+npm run mint
+npm run mint
+npm run mint
+npm run mint
+
+rm -rf .git
+git init
+git add .
+git commit -m "feat: implemented erc721"
+
---
+++ utils/deployed-address.ts
@@ -0,0 +1,3 @@
+const deployedAddress = '0xd36b39Ac280d713F1E9132464fE2D9e32c7bA703'
+
+export default deployedAddress
